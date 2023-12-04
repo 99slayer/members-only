2d9b2f13c329a94508444291d3bda771cad1795a
99slayer <21979862+99slayer@users.noreply.github.com>
2023-12-03 19:22:11 -0700

    Create basic route files

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const userController = require('../controllers/userController');
+
+router.get('/user/detail', userController.user_detail);
+
+router.get('/users', userController.user_list);
+
+router.get('/user/create', userController.user_create_get);
+router.post('/user/post', userController.user_create_post);
 
 module.exports = router;
