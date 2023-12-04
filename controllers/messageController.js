18ef8eea2de749b390cd0176fb190b9274e34b60
99slayer <21979862+99slayer@users.noreply.github.com>
2023-12-03 19:21:34 -0700

    Create basic controller modules

---
+++ controllers/messageController.js
@@ -0,0 +1,15 @@
+const Message = require('../models/message');
+const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+
+exports.message_list = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.message_create_get = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.message_create_post = asyncHandler(async (req, res, next) => {
+
+});
---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+const User = require('../models/user');
+const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+
+exports.user_detail = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.user_list = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.user_create_get = asyncHandler(async (req, res, next) => {
+
+});
+
+exports.user_create_post = asyncHandler(async (req, res, next) => {
+
+});
