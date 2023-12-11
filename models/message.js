5744bcba34456ec5f51daa69b4699c985bcdf25e
99slayer <21979862+99slayer@users.noreply.github.com>
2023-12-11 14:36:06 -0700

    Format message timestamps
    Installed the luxon package and added a virtual to the message schema,
    which returns a formatted timestamp.

--- models/message.js
+++ models/message.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const MessageSchema = new Schema({
 	timestamp: { type: Date, required: true },
 	text: { type: String, required: true },
 	creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+MessageSchema.virtual('creation_date_formatted').get(function () {
+	const dt = DateTime.fromJSDate(this.timestamp);
+	return dt.toLocaleString(DateTime.DATE_SHORT) + ' ' + dt.toLocaleString(DateTime.TIME_WITH_SECONDS);
 });
 
 MessageSchema.virtual('url').get(function () {
